69b757b9f8684d9fb31d4830de5653e604b20eb9
Robert Skarżycki <robert.skarzycki@gmail.com>
2017-10-10 00:42:03 +0200

    Filtry

--- src/Timetable.tsx
+++ src/Timetable.tsx
@@ -8,25 +8,61 @@ import * as cn from 'class-names';
 @observer
 export default class Timetable extends React.Component<{ store: TimetableStore }>
 {
-    componentDidMount(){
+    componentDidMount() {
         this.props.store.fetch();
     }
 
     render() {
         const { store } = this.props;
 
-        const items = store.departures.map(d => <DepartureRow departure={d} key={d.hours.toString()+d.minutes.toString()}/>);
+        const items = store.departures.map(d => <DepartureRow departure={d} key={d.hours.toString() + d.minutes.toString()} />);
 
         return (
             <div>
                 <h3>Odjazdy z dworca Wrocław Główny</h3>
-                <div className={cn({'invisible': !store.isLoading})}>
-                    <Spinner/>
+                <div>
+                    <p>Dokąd:
+                        <input
+                            type="text"
+                            value={store.destination}
+                            onChange={(e) => store.setDestination(e.currentTarget.value)}
+                        /></p>
+                    <p>Po:
+                        <input
+                            type="number"
+                            value={store.afterHours}
+                            onChange={(e) => store.setAfterHours(this.parse(e.currentTarget.value))}
+                        />:<input
+                            type="number"
+                            value={store.afterMinutes}
+                            onChange={(e) => store.setAfterMinutes(this.parse(e.currentTarget.value))}
+                        /></p>
+                    <p>Przed: <input
+                        type="number"
+                        value={store.beforeHours}
+                        onChange={(e) => store.setBeforeHours(this.parse(e.currentTarget.value))}
+                    />:<input
+                            type="number"
+                            value={store.beforeMinutes}
+                            onChange={(e) => store.setBeforeMinutes(this.parse(e.currentTarget.value))}
+                        /></p>
+                </div>
+                <div className={cn({ 'invisible': !store.isLoading })}>
+                    <Spinner />
                 </div>
                 <div>
                     {items}
                 </div>
             </div>
         );
+    }
+
+    private parse(rawValue: string): number {
+        if (!rawValue) {
+            return 0;
+        }
+
+        var parsedValue = parseInt(rawValue);
+        return parsedValue;
     }
 }
--- src/TimetableStore.ts
+++ src/TimetableStore.ts
@@ -5,6 +5,11 @@ export class TimetableStore {
     private apiBaseUrl: string;
     @observable departures: DepartureItem[] = [];
     @observable isLoading: boolean = false;
+    @observable destination: string ='';
+    @observable afterHours: number = 0;
+    @observable afterMinutes: number = 0;
+    @observable beforeHours: number = 23;
+    @observable beforeMinutes: number = 59;
 
     constructor(apiBaseUrl: string) {
         this.apiBaseUrl = apiBaseUrl;
@@ -12,14 +17,51 @@ export class TimetableStore {
 
     fetch(){
         this.isLoading = true;
-        const url = this.apiBaseUrl + 'trains';
+
+        const params = [];
+        
+        //params.push('sleep', 3000);        
+        params.push('fromHours='+(this.afterHours));
+        params.push('fromMinutes='+(this.afterMinutes));
+        params.push('toHours='+(this.beforeHours));
+        params.push('toMinutes='+(this.beforeMinutes));
+
+        let url = this.apiBaseUrl + 'trains';
+        if(params.length > 0) {
+            url+='?'+params.join('&');
+        }
+
         axios.get(url)
         .then(response => {
             this.departures= response.data.departures;
             this.isLoading = false;
         });
-    }  
+    }
+    
+    @action
+    setDestination(destination: string) {
+        this.destination = destination;
+    }
 
+    @action
+    setAfterHours(afterHours: number) {
+        this.afterHours = afterHours;
+    }
+
+    @action
+    setAfterMinutes(afterMinutes: number) {
+        this.afterMinutes = afterMinutes;
+    }
+
+    @action
+    setBeforeHours(beforeHours: number) {
+        this.beforeHours = beforeHours;
+    }
+
+    @action
+    setBeforeMinutes(beforeMinutes: number) {
+        this.beforeMinutes = beforeMinutes;
+    }
 }
 
 export class DepartureItem {
